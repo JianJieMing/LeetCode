427bc9352a57dc248cf038aa0efb78d944c7d795
JianJieMing <1434640043@qq.com>
2020-05-22 17:15:45 +0800

    feat: 最大子序和

---
+++ src/easy/MaxSubArray.java
@@ -0,0 +1,28 @@
+package easy;
+
+/**
+ * 最大子序和
+ *
+ * @author jianjieming
+ * @date 2020/5/22 16:50
+ */
+public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(nums));
+    }
+
+    private static int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+}
